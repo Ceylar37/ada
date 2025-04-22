2c377c1358544e6efd01fd3f13cbbfbb1396396d
ceylar37 <ceylar37@gmail.com>
2025-04-22 17:35:08 +0300

    remove fix: bin

--- bin/resources/stylelint.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: 'stylelint-config-standard-scss',
-  rules: {
-    'selector-class-pattern': ['^[a-z][a-zA-Z0-9]*$', { message: 'Selector should be in camelCase' }]
-  }
-};
